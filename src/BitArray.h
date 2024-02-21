0f38c0e9065f08961ca5640d080a028e778270e5
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-02-21 00:23:48 -0500

    Added docs

--- src/BitArray.h
+++ src/BitArray.h
@@ -8,12 +8,22 @@ template <size_t BitCount>
 class BitArray
 {
 public:
+	/// @brief Get bit count
+	/// @return `size_t` bit count
 	constexpr size_t getBitCount() const { return BitCount; };
 
+	/// @brief Get byte count
+	/// @return `size_t` byte count
 	constexpr size_t getByteCount() const { return getByteCountFromBitCount<BitCount>(); }
 
+	/// @brief Get the state of the bit from given index.
+	/// @param index index of bit
+	/// @return `bool` state of bit at `index`
 	bool get(size_t index) const { return (((bytes[index / 8]) >> (index % 8)) & 0x01); }
 
+	/// @brief Set the state of bit from given index
+	/// @param index index of bit
+	/// @param value value to set
 	void set(size_t index, bool value) { (value ? ((bytes[index / 8]) |= (1UL << (index % 8))) : ((bytes[index / 8]) &= ~(1UL << (index % 8)))); }
 
 private:
